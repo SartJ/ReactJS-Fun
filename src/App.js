fd6b70f6ba53667eab79da1cebe1322d28711199
SartJ <sartajjc@gmail.com>
2021-06-23 09:15:52 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import Tweet from './Tweet';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [users, setUsers] = useState([
+    { name: "John", message: "Go green, clean machine!", likes: 3 },
+    { name: "Tom", message: "No more tears, grizzly bears!", likes: 4 },
+    { name: "Cathy", message: "Chop chop, time to hop on to the top and plant some crops!", likes: 5 }
+  ]);
+
+  const [isRed, setColor] = useState(false);
+
+  const changeColor = () => {
+    setColor(!isRed);
+  }
+
+  return(
+    <div className={isRed ? "red" : "app"}>
+
+      {users.map(user =>(
+        <Tweet name={user.name} tweet={user.message} likes={user.likes} />
+      ))}
+
+      <div className="changerDesign">
+      <h3>Party!</h3>
+      <button onClick={changeColor}>Button</button>
+      </div>
     </div>
   );
 }
+
 
 export default App;
